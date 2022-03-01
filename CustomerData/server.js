b1b4de4e9765c3030992cd99e4c894d94664d393
Hila Shoshan <hilashoshan0605@gmail.com>
2022-03-01 14:45:09 +0200

    added write to database code

--- CustomerData/server.js
+++ CustomerData/server.js
@@ -20,8 +20,8 @@ const db = mysql.createConnection({
     host: "localhost",
     user: "newuser",
     password: "111",
-    database: "call_center"
-})
+    database: "call_center" 
+}) 
  
 db.connect(function (err) {
     if (err) {
@@ -30,12 +30,33 @@ db.connect(function (err) {
     console.log('Connected to the MySQL server.');
 })
 
-db.query("SELECT * FROM call_center.customerData", (err, result, field)=>{
-    if(err) {
-        return console.log(err)
-    }
-    return console.log(result)
-})
+function doQuery(query) {
+    db.query(query, (err, result, field)=>{
+        if(err) {
+            return console.log(err)
+        }
+        return console.log(result)
+    })
+}
+
+/* doQuery("CREATE TABLE customer_data ( \
+            CutomerID int PK, FirstName varchar(45), LastName varchar(45), DateOfBirth date, City varchar(45), Gender tinyint, Internet tinyint, CableTV tinyint, Cellular tinyint);" 
+        ) */
+
+var customers = ["(1111, 'Rony', 'Cohen', '2000-7-04', 'Haifa', 1, 0, 1, 1)",
+                 "(1112, 'Dan', 'Avraham', '1990-4-28', 'Tel-Aviv', 0, 1, 1, 1)",
+                 "(1122, 'Harel', 'Shwarts', '1440-1-01', 'Nahariya', 0, 0, 0, 1)",
+                 "(1222, 'Efrat', 'Saada', '1667-5-13', 'Jerusalem', 1, 1, 1, 1)",
+                 "(2222, 'Simon', 'Levayev', '1992-12-12', 'Bnei-Brak', 0, 1, 1, 1)"]
+
+for (let i = 0; i < customers.length; i++) {
+    doQuery("INSERT INTO call_center.customerdata \
+        (CustomerID, FirstName, LastName, DateOfBirth, City, Gender, Internet, CableTV, Cellular) \
+        VALUES " + customers[i])
+}
+
+doQuery("SELECT * FROM call_center.customerdata")
+
 
 //create connection
 const PORT = process.env.PORT || 3000
