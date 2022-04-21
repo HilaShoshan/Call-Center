3088db9f6a48aaceb37f30e03f0cb60a75122f09
hadar22 <57917375+hadar22@users.noreply.github.com>
2022-04-21 10:52:31 +0300

    Delete routes directory

--- routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const controllers = require("../controllers/controllers.js");
-
-router.get("/dashboard", controllers.getDashboard);
-
-module.exports = router;
